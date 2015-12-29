4450b901611fe418a03f45e5da815470805b634a
Dave Rau <dave@redlabor.com>
2015-12-28 19:39:12 -0800

    failed attempt to render champs list

--- app/client/templates/home/home.js
+++ app/client/templates/home/home.js
@@ -12,6 +12,21 @@ Template.Home.events({
 /* Home: Helpers */
 /*****************************************************************************/
 Template.Home.helpers({
+  // allChamps list
+  // should be working: https://github.com/meteor/meteor/blob/devel/packages/spacebars/README.md#each
+  allChamps: function () {
+    Meteor.call('getChamps', function (err, results) {
+      // turn object into array for template to consume
+      var result = [];
+      for (var key in results.data.data) {
+        result.push( { name: key, data: results.data.data[key] } );
+      }
+      // console shows proper array of objects with data
+      console.log(result);
+      return result;
+    });
+  },
+
   selectedChamp: function () {
     var champ = Session.get('champ');
     Meteor.call('getChamps', champ, function (err, results) {
