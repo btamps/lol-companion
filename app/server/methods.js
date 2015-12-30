c425af20986a641721418c7c0210c01add3d0a7f
Billy Tamplin <billytamplin@gmail.com>
2015-12-30 08:09:52 -0800

    made changes to the champ vars

--- app/server/methods.js
+++ app/server/methods.js
@@ -4,33 +4,32 @@
 
 Meteor.methods({
   getChamps: function() {
-        // Get champions from Riot server
-        HTTP.get('http://ddragon.leagueoflegends.com/cdn/5.24.2/data/en_US/champion.json', function(error, riotResponse) {
-            var champs = [];
-            var entries = riotResponse.data.data;
-            var sort = 0;
-            _.each(entries, function(entry) {
-                var champ = {};
-                champ.id = entry.key;
-                champ.name = entry.name;
-                champ.title = entry.title;
-                champ.blurb = entry.blurb;
-                champ.imageUrl = "http://ddragon.leagueoflegends.com/cdn/5.24.2/img/champion/" + entry.image.full;
-                champ.tags = entry.tags[0];
+    // Get champions from Riot server
+    HTTP.get('http://ddragon.leagueoflegends.com/cdn/5.24.2/data/en_US/champion.json', function(error, riotResponse) {
+      var champs = [];
+      var entries = riotResponse.data.data;
+      _.each(entries, function(entry) {
+        var champ = {};
+        champ.id = entry.key;
+        champ.name = entry.name;
+        champ.title = entry.title;
+        champ.blurb = entry.blurb;
+        champ.imageUrl = "http://ddragon.leagueoflegends.com/cdn/5.24.2/img/champion/" + entry.image.full;
+        champ.tags = entry.tags[0];
 
-                // Add champ to list
-                champs.push(champ);
-            }, this);
+        // Add champ to list
+        champs.push(champ);
+      }, this);
 
-            // Delete all existing champions from database
-            Champions.find().forEach(function(champ) {
-                Champions.remove(champ._id);
-            });
+      // Delete all existing champions from database
+      Champions.find().forEach(function(champ) {
+        Champions.remove(champ._id);
+      });
 
-            // Insert new champions from array into database
-            _.each(champs, function(champ) {
-                Champions.upsert(champ._id, champ);
-            }, this);
-        });
-    }
+      // Insert new champions from array into database
+      _.each(champs, function(champ) {
+        Champions.upsert(champ._id, champ);
+      }, this);
+    });
+  }
 });
