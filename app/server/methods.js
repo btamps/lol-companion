af17b368e9f1cbe4a3f0e56415913e76c382fd5e
Billy Tamplin <billytamplin@gmail.com>
2016-01-10 01:59:20 -0800

    Added champ infos to game history

--- app/server/methods.js
+++ app/server/methods.js
@@ -1,7 +1,7 @@
 /*****************************************************************************/
 /*  Server Methods */
 /*****************************************************************************/
-
+var Future = Npm.require( 'fibers/future' );
 Meteor.methods({
   getChamps: function() {
     // Get champions from Riot server
@@ -70,6 +70,7 @@ Meteor.methods({
     // Get recent champions from Riot server
     HTTP.get('https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/50899122/recent?api_key=e29194e0-9998-440f-bf4f-ae7dca4fadb5', function(error, riotResponse) {
       var games = [];
+      var recentChamps = [];
       var entries = riotResponse.data.games;
       _.each(entries, function(entry) {
         var game = {};
@@ -80,38 +81,21 @@ Meteor.methods({
         game.deaths = entry.stats.numDeaths;
         game.assists = entry.stats.assists;
         game.isWin = entry.stats.win;
+        game.champInfo = Champions.findOne({'id': String(entry.championId)});
 
         // Add game to list
         games.push(game);
 
       }, this);
-
+      console.log(recentChamps);
       // Delete all existing champions from database
       Games.find().forEach(function(game) {
-        Games.remove(game._id);
+          Games.remove(game._id);
       });
 
       // Insert new champions from array into database
       _.each(games, function(game) {
         Games.upsert(game._id, game);
-      }, this);
-
-    });
-
-    // Get Champion data from Riot server
-    HTTP.get('http://ddragon.leagueoflegends.com/cdn/5.24.2/data/en_US/champion.json', function(error, riotResponse) {
-      var champs = [];
-      var entries = riotResponse.data.data;
-      _.each(entries, function(entry) {
-        
-        // Checks to see if champ ids match and sets the props
-        Games.update({ 'championId': Number(entry.key) }, {
-    			$set: {
-    				name: entry.name,
-    				imageUrl: "http://ddragon.leagueoflegends.com/cdn/5.24.2/img/champion/" + entry.image.full
-    			}
-    		},{ multi: true });
-
       }, this);
 
     });
